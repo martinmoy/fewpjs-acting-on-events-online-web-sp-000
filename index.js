f37b757786e07be194d24a274452404040f2df9b
Martin Moy <martinmoy1991@gmail.com>
2020-03-22 12:54:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
-
     dodger.style.left = `${left - 1}px`;
   }
-
-  else {
+  else 
      (e.key === "ArrowRight") {
       let rightNumbers = dodger.style.right.replace("px", "");
       let right = parseInt(rightNumbers, 10);
-
       dodger.style.right = `${right + 1}px`;
   }
 });

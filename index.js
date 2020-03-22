f6d9dc61a237403ce47c343b7d0e9bcbc74d36e1
Martin Moy <martinmoy1991@gmail.com>
2020-03-22 13:18:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function moveDodgerRight() {
     dodger.style.left = `${left + 1}px`;
   }
 }
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});

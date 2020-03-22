dfa597654085167e4a400fa103e0e91d1cc0e9bd
Martin Moy <martinmoy1991@gmail.com>
2020-03-22 13:01:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function moveDodgerRight() {
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+    moveDodgerLeft();}
 
   else (e.key ==="ArrowRight"){
     moveDodgerRight();

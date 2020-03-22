d6264dc664df30d310c2c247c548fbcd567cfb71
Martin Moy <martinmoy1991@gmail.com>
2020-03-22 12:53:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
- 
+
     dodger.style.left = `${left - 1}px`;
   }
 
@@ -11,9 +11,7 @@ document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowRight") {
       let rightNumbers = dodger.style.right.replace("px", "");
       let right = parseInt(rightNumbers, 10);
-   
+
       dodger.style.right = `${right + 1}px`;
   }
 });
-
-
